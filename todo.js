fc7a3857f9e99b7a63d88f9121ca4e856cebf09a
Maria <122727669+MariaFeli@users.noreply.github.com>
2023-05-08 22:28:44 +0200

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,34 +1,30 @@
-(function($) {
-  'use strict';
-  $(function() {
-    var todoListItem = $('.todo-list');
-    var todoListInput = $('.todo-list-input');
-    $('.todo-list-add-btn').on("click", function(event) {
-      event.preventDefault();
+$(document).ready(function(){
+    
+ $("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
 
-      var item = $(this).prevAll('.todo-list-input').val();
 
-      if (item) {
-        todoListItem.append("<li><div class='form-check'><label class='form-check-label'><input class='checkbox' type='checkbox'/>" + item + "<i class='input-helper'></i></label></div><i class='remove mdi mdi-close-circle-outline'></i></li>");
-        todoListInput.val("");
-      }
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
 
-    });
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		
+		var todoText = $(this).val();
+		$(this).val("");
+	
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+	}
+});
 
-    todoListItem.on('change', '.checkbox', function() {
-      if ($(this).attr('checked')) {
-        $(this).removeAttr('checked');
-      } else {
-        $(this).attr('checked', 'checked');
-      }
-
-      $(this).closest("li").toggleClass('completed');
-
-    });
-
-    todoListItem.on('click', '.remove', function() {
-      $(this).parent().remove();
-    });
-
-  });
-})(jQuery);
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});   
+    
+    
+});
